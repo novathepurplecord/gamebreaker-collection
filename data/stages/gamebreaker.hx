31d4fdf6285c1d63e2418966f79565440606f524
novathepurplecord <mesterdani210@gmail.com>
2026-01-09 18:19:52 +0200

    gamebreaker v2

--- data/stages/gamebreaker.hx
+++ data/stages/gamebreaker.hx
@@ -5,10 +5,15 @@ function beatHit(_:Int) {
             case 72: for (obj in [bg, hill, trees]) FlxTween.color(obj, 2, 0xFFFFFFFF, 0xFFFFAAAA, {ease: FlxEase.quadInOut});
             case 204: for (obj in stage.stageSprites) obj.color = 0xFFFF3333;
         }
-    } else {
+    } else if (PlayState.SONG.meta.name == 'impersonator') {
         switch (_) {
             case 108: for (obj in [bg, hill, trees]) FlxTween.color(obj, 2, 0xFFFFFFFF, 0xFFFFAAAA, {ease: FlxEase.quadInOut});
             case 176: for (obj in stage.stageSprites) obj.color = 0xFFFF3333;
+        }
+    } else if (PlayState.SONG.meta.name == 'gamebreaker-v2') {
+        switch (_) {
+            case 72: for (obj in [bg, hill, trees]) FlxTween.color(obj, 2, 0xFFFFFFFF, 0xFFFFAAAA, {ease: FlxEase.quadInOut});
+            case 204: for (obj in stage.stageSprites) obj.color = 0xFFFF3333;
         }
     }
 }
